92fa8d284f98a1955e69e4c9c19675a46819154a
Martin Blasco <martin.blasco@eventbrite.com>
2019-09-04 12:53:17 -0300

    new settings

---
+++ todo_list/settings/dev.py
@@ -0,0 +1,6 @@
+from .base import *
+
+SOCIAL_AUTH_EVENTBRITE_KEY = get_env_variable('SOCIAL_AUTH_EVENTBRITE_KEY')
+SOCIAL_AUTH_EVENTBRITE_SECRET = get_env_variable(
+    'SOCIAL_AUTH_EVENTBRITE_SECRET',
+)
