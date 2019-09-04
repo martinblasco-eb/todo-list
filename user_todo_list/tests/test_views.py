b2c60bb37a72b7e09e52a6e1171680c19960d336
Martin Blasco <martin.blasco@eventbrite.com>
2019-09-04 09:31:03 -0300

    adding tests for views

--- user_todo_list/tests/test_views.py
+++ user_todo_list/tests/test_views.py
@@ -12,7 +12,7 @@ from datetime import datetime
 class TestView(TestCase):
 
     def setUp(self):
-        self.user=User.objects.create_user(username='testuser2', password='foo')
+        self.user=User.objects.create_user(username='testuser', password='foo')
         UserSocialAuth.objects.create(
             user=self.user,
             provider='eventbrite',
@@ -26,12 +26,12 @@ class TestView(TestCase):
 
     def test_user_logged_in(self):
         client = Client()
-        response = client.post('/accounts/login/', {'username': 'testuser2', 'password':'foo'})
+        response = client.post('/accounts/login/', {'username': 'testuser', 'password':'foo'})
         self.assertEqual(response.status_code, 302)
 
     def test_user_fail_logged_in(self):
         client = Client()
-        response = client.post('/accounts/login/', {'username': 'testuser3', 'password':'foo'})
+        response = client.post('/accounts/login/', {'username': 'testuser2', 'password':'foo'})
         self.assertEqual(response.status_code, 200)
 
     def test_task_create(self):
@@ -41,16 +41,33 @@ class TestView(TestCase):
         data =  {'name': 'task', 'priority':priority}
         response = self.client.post(url, data)
         self.assertEqual(response.status_code, 301)
-        
+ 
+    def test_task_update(self):
+        priority = Priority.objects.create(name='low')
+        date = datetime.now()
+        url = '/task/{}/update/{}'.format('23221','10')
+        data =  {'name': 'task', 'priority':priority}
+        response = self.client.post(url, data)
+        self.assertEqual(response.status_code, 301)
 
-    # def test_empty_get(self):
-    #     eventbrite = Eventbrite(UserSocialAuth.objects.get(pk=1))
-    #     response = eventbrite.get('/dev/me/events')
-    #     self.assertEqual(response.status_code, 404)
-    # # def test_event_list(self):
-    # @patch('user_todo_list.views.Eventbrite.get', return_value=dict)
-    # def test_get_events(self):
-    #     login = self.cliente.force_login(self.user)
-    #     self.client.get('/events/')
-    #     mocked_get.assert_called_with('/users/me/events?page_size=5&page=4')
+    def test_task_delete(self):
+        priority = Priority.objects.create(name='low')
+        date = datetime.now()
+        url = '/task/{}/delete/{}'.format('23221','10')
+        data =  {'name': 'task', 'priority':priority}
+        response = self.client.post(url, data)
+        self.assertEqual(response.status_code, 301)
 
+    def test_check_task(self):
+        url = '/task/{}/check/{}'.format('23221','10')
+        data =  {'name': 'task'}
+        response = self.client.post(url, data)
+        self.assertEqual(response.status_code, 301)
+
+    def test_empty_get(self):
+        eventbrite = Eventbrite(UserSocialAuth.objects.get(pk=1))
+        response = eventbrite.get('/user/events/me')
+        self.assertEqual(response.status_code, 404)
+
+    def test_event_list(self):
+        pass
